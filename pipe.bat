ce3bea0bae23a12490fd4c3b7e9002a975d2008f
Bujji123 <ambadipudi.nagalaxmi@gmail.com>
2026-08-16 17:52:30 +0530

    modified the package and pipe file

---
+++ pipe.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo ============================
+echo Installing Dependencies...
+echo ============================
+
+call npm install
+
+echo ============================
+echo Installing Playwright Browsers
+echo ============================
+
+call npx playwright install
+
+echo ============================
+echo Running Playwright Tests...
+echo ============================
+
+call npx playwright test
+
+echo ============================
+echo Test Execution Completed
+echo ============================
+
+exit /b %errorlevel%
