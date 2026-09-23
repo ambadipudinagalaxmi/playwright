8397b5af4e8862caa3ff403c3e914dc318e50559
Bujji123 <ambadipudi.nagalaxmi@gmail.com>
2026-09-23 10:49:35 +0530

    modified the Amazon1 tets

--- tests/Amazon1.spec.ts
+++ tests/Amazon1.spec.ts
@@ -1,6 +1,6 @@
 import {test , expect} from '@playwright/test';
 test('amazon', async({page})=>{
     await page.goto("https://www.amazon.in/");
-    await page.getByRole('link', {name:"Today's Deals"}).hover()
+    await page.getByRole('link', {name:"Today's Deals"}).click();
 await page.waitForTimeout(3000);
 });
